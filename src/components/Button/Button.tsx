ead5dfd81566c9c84a1a89bdf2727f274d7b20c6
iabdulkader <abdulkadernow@gmail.com>
2023-01-01 23:43:33 +0600

    Minor Upgrade

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -15,18 +15,18 @@ export default function Button({
         <button 
           onClick={onClick}
           disabled={loading}
-          className={`button group flex justify-center items-center h-${height} w-${width}`}
+          className={`button group flex justify-center items-center w-${width} h-${height}`}
         >
           {loading ? 
           (
             <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" width="24" height="22">
-                <path className="fill-slate-900 group-hover:fill-slate-50 dark:fill-slate-50 dark:group-hover:fill-slate-900" d="M0 0h4v10H0z">
+                <path className="fill-slate-900 group-hover:fill-slate-50 dark:fill-slate-50 dark:group-hover:fill-slate-900" d="M0 0h2v8H0z">
                     <animateTransform attributeName="transform" attributeType="xml" begin="0" dur="0.6s" repeatCount="indefinite" type="translate" values="0 0; 0 15; 0 0"/>
                 </path>
-                <path className="fill-slate-900 group-hover:fill-slate-50 dark:fill-slate-50 dark:group-hover:fill-slate-900" d="M10 0h4v10h-4z">
+                <path className="fill-slate-900 group-hover:fill-slate-50 dark:fill-slate-50 dark:group-hover:fill-slate-900" d="M10 0h2v8h-2z">
                     <animateTransform attributeName="transform" attributeType="xml" begin="0.2s" dur="0.6s" repeatCount="indefinite" type="translate" values="0 0; 0 15; 0 0"/>
                 </path>
-                <path className="fill-slate-900 group-hover:fill-slate-50 dark:fill-slate-50 dark:group-hover:fill-slate-900" d="M20 0h4v10h-4z">
+                <path className="fill-slate-900 group-hover:fill-slate-50 dark:fill-slate-50 dark:group-hover:fill-slate-900" d="M20 0h2v8h-2z">
                     <animateTransform attributeName="transform" attributeType="xml" begin="0.4s" dur="0.6s" repeatCount="indefinite" type="translate" values="0 0; 0 15; 0 0"/>
                 </path>
             </svg>
--- src/pages/files/[pin].tsx
+++ src/pages/files/[pin].tsx
@@ -1,7 +1,29 @@
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 import Files from "../../components/Files/Files";
 import PinHolder from "../../components/PinHolder/PinHolder";
+import { UserContext } from "../../context/userContext/userContext";
 
 export default function Pin(){
+    const router = useRouter();
+    const { updatePin } = useContext(UserContext);
+    const { pin } = router.query;
+
+    useEffect(() => {
+        console.log(isNaN(parseFloat(pin as string)))
+        if(pin && (pin?.length < 8 || pin.length > 8 || isNaN(pin as any))){
+            router.push("/");
+        }
+        if (pin && updatePin) {
+            updatePin(pin as string);
+        };
+    }, [pin])
+    
+    
+
+    
+    
+
     return(
         <div className="flex flex-col lg:flex-row mt-16">
             <div className="w-full max-w-[450px] h-full mx-auto my-3 lg:mt-6 lg:mb-20">
