9acc48f3bdeacd9ff2db15223dcdd4c98359fe17
iabdulkader <abdulkadernow@gmail.com>
2023-01-03 02:19:11 +0600

    Typo 🤑

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 
 export default function MetaHead({ title }: { title?: string}) {
+
     return (<Head>
-                <title>[title]</title>
+                <title>{title}</title>
                 <meta name="description" content="JoShare is a simple file sharing app." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
