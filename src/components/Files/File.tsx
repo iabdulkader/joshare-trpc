9601bb768ac8d7955670fa2f8c9d775ad2801a45
iabdulkader <abdulkadernow@gmail.com>
2023-01-24 21:39:46 +0600

    File Hover

--- src/components/Files/File.tsx
+++ src/components/Files/File.tsx
@@ -36,7 +36,7 @@ export default function File({ file, auth = true }: { file: FileType, auth?: boo
 
     return(
         <div className="mb-2 w-full flex justify-center">
-            <div className="w-full bg-secondaryBg-light dark:bg-secondaryBg-dark flex py-3 rounded-lg">
+            <div className="w-full bg-secondaryBg-light dark:bg-secondaryBg-dark flex py-3 rounded-lg hover:bg-stone-300 hover:dark:bg-slate-700 duration-300 transition-all">
 
               <div className='ml-4 flex items-center h-[22px] w-[22px] my-auto'>
                 <FileIcon extension={file.ext} {...iconStyles} key={file.id} />
