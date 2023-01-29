5d30b35b72354482e5d882db57d243fc5887457c
iabdulkader <abdulkadernow@gmail.com>
2023-01-29 19:37:17 +0600

    Minor fix

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -17,7 +17,7 @@ export default function About() {
 
             <div className="flex justify-center mb-6">
 
-              <div className="columns-1 max-w-[1000px] lg:columns-2 gap-6 px-0 pr-5 lg:px-8">
+              <div className="columns-1 max-w-[1000px] lg:columns-2 gap-6 px-0 lg:px-8">
                 <div className="container">
                   <h1>What is joShare ?</h1>
                   <p>joShare is a micro file sharing platform. Now what do I mean by micro file sharing??</p>
