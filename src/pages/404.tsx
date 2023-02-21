e4447f9d71d0a730c9a1fcb77dec7569b88a2568
iabdulkader <abdulkadernow@gmail.com>
2023-02-21 23:36:02 +0600

    Minor

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,18 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 import errorImg from "../assets/error.svg";
 import Button from "../components/Button/Button";
+import MetaHead from "../components/Head/Head";
 import Layout from "../components/Layout/Layout";
 
 export default function Custom404() {
   return (
-    <Layout>
-      <div className="min-h-[calc(100vh-11rem)] flex flex-col justify-center items-center my-auto px-8 lg:px-0">
-        <Image src={errorImg} alt="Error" />
+    <>
+      <MetaHead title="Not Found | File Sharing Solution" />
+      <Layout>
+        <div className="min-h-[calc(100vh-11rem)] flex flex-col justify-center items-center my-auto px-8 lg:px-0">
+          <Image src={errorImg} alt="Error" />
 
-        <Link className="mt-14 lg:mt-10" href="/">
-          <Button height={10} width={28} text="Home" />
-        </Link>
-      </div>
-    </Layout>
+          <Link className="mt-14 lg:mt-10" href="/">
+            <Button height={10} width={28} text="Home" />
+          </Link>
+        </div>
+      </Layout>
+    </>
   );
 }
